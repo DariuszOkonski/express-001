b444da403e43adc8a825adc2d063fd4d00d9f258
Dariusz Okonski <darek200180@gmail.com>
2019-07-28 17:22:49 +0100

    menu item

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-// GET home page
-
 router.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Express'
-  });
+  res.render('admin', {
+    title: 'Admin'
+  })
 });
 
 module.exports = router;
